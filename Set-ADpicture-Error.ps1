2be49443c887edc356cbd37fdd5074cffd83b822
SharePointSan <NL12143@hotmail.com>
2017-12-22 13:40:30 +0100

    Create Set-ADpicture-Error.ps1

---
+++ Set-ADpicture-Error.ps1
@@ -0,0 +1,32 @@
+#Set-ADpicture-Error.ps1
+#Module to generate error to log
+
+$StartDir = "C:\Users\Public\AccountPictures\"
+$LogFile = "$StartDir\Set-ADpicture-AG-Log.log"  
+$LogFile
+$Username = "TestUserName" 
+$UserSID = "TestUserSID" 
+$Error | Get-Member  
+$Error.Exception.Message
+
+TRY { 
+#Log when TRY done 
+$text1 = "$(Get-Date -format yyyy-MM-dd-HH:mm:ss). No error running picture script."    
+$text2 = "Pictures for user $UserName stored in C:\Users\Public\AccountPictures\$UserSID" 
+Set-Content $text1 -Path $LogFile 
+Add-Content $text2 -Path $LogFile 
+}
+
+CATCH {
+#Log when catch
+$LogFile = "$StartDir\Set-ADPicture-AG-Log.log"  
+If ($Error) {
+Set-Content $(Get-Date -format yyyy-MM-dd-HH:mm:ss) –path $LogFile -ErrorAction SilentlyContinue 
+Add-Content "Errors when running the picture script. Details below:" –path $LogFile -ErrorAction SilentlyContinue
+Add-Content $Error.Exception.Message -Path $LogFile -ErrorAction SilentlyContinue 
+} 
+Else { Set-Content "Catch but no error $(Get-Date -format yyyy-MM-dd-HH:mm:ss)" –path $LogFile }
+} 
+#endregion CATCH 
+
+#Add for production: -ErrorAction SilentlyContinue
