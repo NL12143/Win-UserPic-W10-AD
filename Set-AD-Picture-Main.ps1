b1b7ff70d2f3af33527694612cfa8c0d68b702df
SharePointSan <NL12143@hotmail.com>
2017-12-29 14:17:29 +0100

    Update Set-AD-Picture-Main.ps1

--- Set-AD-Picture-Main.ps1
+++ Set-AD-Picture-Main.ps1
@@ -7,20 +7,19 @@
 #edits for company and added image resizing using function 
 #authors: Roeland Cerfonteijn; Sencer Demir 
 
-TRY {
-#region Set script variables 
+#Set script variables 
 $Error.Clear()
-$StartDir = "C:\Users\Public\AccountPictures" 
-#$StartDir = "C:\Program Files (x86)\Internal\Workplace\AcountPicture"
-
+$StartDir = "C:\Users\Public\Set-ADpicture" 
 Set-Location $StartDir  
 Import-Module .\Set-ADpicture-Resize.ps1 
 $DefaultPic = "$StartDir\Set-ADpicture-Default.jpg" 
 $LogFile = "$StartDir\Set-ADpicture-Log.log"  
+
 #Get user object from AD and store in script variables 
-$user = ([ADSISearcher]"(&(objectCategory=User)(SAMAccountName=$env:username))").FindOne().Properties
-$userName = $env:username
-$userSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+##$user = ([ADSISearcher]"(&(objectCategory=User)(SAMAccountName=$env:username))").FindOne().Properties
+##$userSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+$user = Get-ADuser -Identity $env:username -Properties * 
+$userSID = $user.SID.Value 
 
 #Set picture to ADpicture or default 
 If ($user.thumbnailphoto -eq $null) { 
@@ -50,6 +49,8 @@ $image_base =  "C:\Users\Public\AccountPictures"
 #C:\Users\Public\AccountPictures\<User_SID>\ as when user changes himself
 $dir = $image_base + "\" + $userSID
 If ((Test-Path -Path $dir) -eq $false) { $(mkdir $dir).Attributes = "Hidden" }
+$imageAD = $dir + "\" + "imageAD.jpg" 
+
 #Prepare registry keys for AccountPictures
 #HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users\
 $reg_base = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users\{0}"
@@ -57,12 +58,12 @@ $reg_base = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\User
 $reg_key = [string]::format($reg_base, $userSID)
 $reg_value_mask = "Image{0}"
 If ((Test-Path -Path $reg_key) -eq $false) { New-Item -Path $reg_key } 
-#endregion
+
 #Save photo imported from AD 
-$imageAD = $dir + "\" + "imageAD.jpg" 
 $userphoto | Set-Content -Path $imageAD -Encoding Byte -Force 
-$userphoto
+
 #region Loop for picture sizes
+TRY {
 ForEach ($size in $image_sizes) {
     #Save image to disk C:\Users\Public\AccountPictures\<User_SID>\ 
     $file_name = ([string]::format($image_mask, $size))
@@ -72,27 +73,16 @@ ForEach ($size in $image_sizes) {
     $name = [string]::format($reg_value_mask, $size)
     $value = New-ItemProperty -Path $reg_key -Name $name -Value $path -Force
 }
-#endregion loop
-
-#region logging
+#Logging
 $text1 = "$(Get-Date -format yyyy-MM-dd-HH:mm:ss). No error running picture script."    
 $text2 = "Pictures for user $UserName stored in C:\Users\Public\AccountPictures\$UserSID" 
 Set-Content $text1 -Path $LogFile -ErrorAction SilentlyContinue
 Add-Content $text2 -Path $LogFile -ErrorAction SilentlyContinue
-#endregion logging 
 }
-#endregion TRY
 
-#region CATCH 
 CATCH { 
 If ($Error) {
 Set-Content $(Get-Date -format yyyy-MM-dd-HH:mm:ss) –path $LogFile -ErrorAction SilentlyContinue 
 Add-Content $Error.Exception.Message -Path $LogFile -ErrorAction SilentlyContinue }
 Else {Set-Content "Catch but no error" $(Get-Date -format yyyy-MM-dd-HH:mm:ss) –path $LogFile }
-#endregion CATCH
 
-#ToDo 
-#Add for production: -ErrorAction SilentlyContinue
-#Add a default picture when user has no thumbnail in AD.
-#Add a catch in case the loop fails. 
-#Add a time-out value to quit after 10 ms.
